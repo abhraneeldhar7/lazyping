941c14aa52b233430fb68f3651e05a88338250f9
Abhraneel <abhraneeldhar7@gmail.com>
2026-01-30 11:00:28 +0530

    lil fix

--- app/(authpage)/sso-callback/page.tsx
+++ app/(authpage)/sso-callback/page.tsx
@@ -1,9 +1,14 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
+import { LoaderCircleIcon } from "lucide-react";
 
 export default function SSOCallback() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center h-screen px-[15px]">
             <AuthenticateWithRedirectCallback />
+            <div className="flex items-center gap-[15px]">
+                <LoaderCircleIcon size={26} className="animate-spin" />
+                <h1 className="text-center text-[24px] animate-pulse">Signin in...</h1>
+            </div>
         </div>
     );
 }
